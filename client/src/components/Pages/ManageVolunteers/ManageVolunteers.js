ab61d0b0358c708fc57aa49b569d23756f05919a
Lyra Freund <lyrafreund@gmail.com>
2023-12-01 13:38:45 -0700

    quick fix

--- client/src/components/Pages/ManageVolunteers/ManageVolunteers.js
+++ client/src/components/Pages/ManageVolunteers/ManageVolunteers.js
@@ -6,7 +6,7 @@ const ManageVolunteers = ({ volunteers }) => {
   return (
     <>
     <div className='entire-volunteer-pg'>
-      <h1 className='home-header'>Manage Volunteers</h1>
+      <h1 className='home-header'>View Volunteers</h1>
       <div className='volunteer-container'>
         {volunteers.map((volunteer) => (
               <VolunteerCard key={volunteer.id} name={volunteer.name} email={volunteer.email} registrations={volunteer.registrations}/>
